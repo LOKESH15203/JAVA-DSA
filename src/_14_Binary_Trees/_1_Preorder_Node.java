e3af6e74923fb616e27c76115333706db70a00d1
Lokesh Sonawane <omsonawane15203@gmail.com>
2023-06-08 09:42:50 +0530

    CORRECT  Pre In and Post ORDER for the tree built

--- src/_14_Binary_Trees/_1_Preorder_Node.java
+++ src/_14_Binary_Trees/_1_Preorder_Node.java
@@ -43,16 +43,16 @@ public class _1_Preorder_Node {
             if(root == null){
                 return;
             }
-            preTraversal(root.left);
+            InTraversal(root.left);
             System.out.print(root.data + " ");
-            preTraversal(root.right);
+            InTraversal(root.right);
         }
         public static void postTraversal(Node root){
             if(root == null){
                 return;
             }
-            preTraversal(root.left);
-            preTraversal(root.right);
+            postTraversal(root.left);
+            postTraversal(root.right);
             System.out.print(root.data + " ");
         }
     }
@@ -61,7 +61,7 @@ public class _1_Preorder_Node {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.println(root.data);
+
         tree.preTraversal(root);
         System.out.println();
         tree.InTraversal(root);
