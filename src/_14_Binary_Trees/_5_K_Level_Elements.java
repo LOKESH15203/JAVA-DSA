4f071e7912f7517ab6cd84f4b2476ffe0fa1c420
Lokesh Sonawane <omsonawane15203@gmail.com>
2023-06-14 21:49:41 +0530

    Element at Kth Level Iterative

---
+++ src/_14_Binary_Trees/_5_K_Level_Elements.java
@@ -0,0 +1,73 @@
+package _14_Binary_Trees;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class _5_K_Level_Elements {
+
+    static class Node {
+        int data;
+        Node right;
+        Node left;
+
+        public Node(int val) {
+            this.data = val;
+            this.right = null;
+            this.left = null;
+        }
+    }
+
+    public static void K_th_Elem(int n, Node root) {
+        if (root == null) {
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        int count = 1;
+        while (!q.isEmpty()) {
+            Node currNode = null;
+            if (currNode == null) {
+                if (q.isEmpty()) {
+                    count++;
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                if (count == n) {
+                    System.out.println(currNode.data + " ");
+                }
+                if (currNode.left != null) {
+                    q.add(currNode.left);    // and left child of the currNode
+                }
+                if (currNode.right != null) {
+                    q.add(currNode.right);
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        /*
+                      1
+                    /   \
+                  2      3
+                /  \   /  \
+              4    5  6    7
+         */
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        K_th_Elem(2, root);
+    }
+}
