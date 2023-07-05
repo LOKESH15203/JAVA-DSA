8eb2c294c71f56cc60e43ff164992fa15b6a0185
Lokesh Sonawane <omsonawane15203@gmail.com>
2023-07-05 06:31:34 +0530

    Majority element in an array using hash maps. This reduces the Time Complexity from O(n^2)-> Brute Force Approch to O(n) -> Hashing

--- src/_17_Hashing/_3_HashMap_Implementation.java
+++ src/_17_Hashing/_3_HashMap_Implementation.java
@@ -4,32 +4,31 @@ import _10_Linked_List.LinkedList;
 
 import javax.annotation.processing.SupportedAnnotationTypes;
 
-public class _3_HashMap_Implementation {
-
-    static class HashMap<K,V>{               // Generic - the data-types are kept generic
-        private class Node{
-            K key;
-            V value;
-
-            public Node(K key, V value){
-                this.key = key;
-                this.value = value;
-            }
-        }
-
-        private int size;
-//        private LinkedList<Node> buckets[];         // This means array storing LikedList values
-//      private        int       arr[];                This means array storing int values
+//public class _3_HashMap_Implementation {
 //
-//        public HashMap(){
-//            this.size = 0;
-//            this.buckets = new LinkedList[4];
-//            for(int i=0; i<4; i++){
-//                this.buckets[i] = new LinkedList<>();
+//    static class HashMap<K,V>{               // Generic - the data-types are kept generic
+//        private class Node{
+//            K key;
+//            V value;
+//
+//            public Node(K key, V value){
+//                this.key = key;
+//                this.value = value;
 //            }
 //        }
-    }
-    public static void main(String[] args) {
-
-    }
-}
+//
+//        private int size;
+////        private LinkedList<Node> buckets[];         // This means array storing LikedList values
+////      private        int       arr[];                This means array storing int values
+////
+////        public HashMap(){
+////            this.size = 0;
+////            this.buckets = new LinkedList[4];
+////            for(int i=0; i<4; i++){
+////                this.buckets[i] = new LinkedList<>();
+////            }
+////        }
+//    }
+//    public static void main(String[] args) {
+//    }
+//}
