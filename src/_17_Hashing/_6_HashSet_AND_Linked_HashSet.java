4566c7f33a99294be347974c999e202a463c34b6
Lokesh Sonawane <omsonawane15203@gmail.com>
2023-07-06 20:53:33 +0530

    Tree Set Implementation

--- src/_17_Hashing/_6_HashSet_AND_Linked_HashSet.java
+++ src/_17_Hashing/_6_HashSet_AND_Linked_HashSet.java
@@ -2,18 +2,19 @@ package _17_Hashing;
 
 import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.TreeSet;
 
 public class _6_HashSet_AND_Linked_HashSet {
 
 
     public static void main(String[] args) {
 
-        HashSet<Integer> hs = new HashSet<>();
+        HashSet<String> hs = new HashSet<>();
 
-        hs.add(1);
-        hs.add(5);
-        hs.add(2);
-        hs.add(6);
+        hs.add("Delhi");
+        hs.add("Mumbai");
+        hs.add("Noida");
+        hs.add("Bengaluru");
         System.out.println(hs);
 
         System.out.println(hs.contains(1));
@@ -30,5 +31,14 @@ public class _6_HashSet_AND_Linked_HashSet {
 
         System.out.println(lhs);
         // All operations of HashSet can be performed on likedHashSet
+
+
+        TreeSet<String> ts = new TreeSet<>();   // Sorts the input in ascending order
+        ts.add("Delhi");
+        ts.add("Mumbai");
+        ts.add("Noida");
+        ts.add("Bengaluru");
+
+        System.out.println(ts);
     }
 }
