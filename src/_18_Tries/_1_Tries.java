42988f2b42a712adfeddccd8c2b4c2c931fb999d
Lokesh Sonawane <omsonawane15203@gmail.com>
2023-07-10 08:15:51 +0530

    Insert in Tries

--- src/_18_Tries/_1_Tries.java
+++ src/_18_Tries/_1_Tries.java
@@ -4,7 +4,7 @@ public class _1_Tries {
 
     static class Node {
         Node children[] = new Node[26];    // 26 alphabets
-        boolean eow = false;               // End of Row = eow
+        boolean eow = false;               // End of Word = eow
 
         Node(){
             for(int i=0; i<26; i++){
@@ -15,7 +15,25 @@ public class _1_Tries {
 
     public static Node root = new Node();         // This is always empty
 
+    public static void insert(String word){
+        Node curr = root;
+        for(int level=0; level<word.length(); level++){
+            int idx = word.charAt(level) - 'a';             // Here if word.charAt(level) = 'a' we get a-a = 0(Zero) i.e.
+            if(curr.children[idx] == null){
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
     public static void main(String[] args) {
+
+        String words[] = {"the", "a", "there", "their", "any", "thee"};
+        for (int i=0; i<words.length; i++){
+            insert(words[i]);
+        }
+
 
     }
 }
